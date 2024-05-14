2dfa630309a127d434784853ecc808b3b093a6a7
dtDhruv <dhruvthanawala10@gmail.com>
2024-05-14 16:49:12 +0530

    switched to httpx and added requirements

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
-import aiohttp
+import httpx
 import asyncio
 import time
+from bs4 import BeautifulSoup
+import lxml
 
 async def fetch_fixtures(URL):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(URL) as response:
-            html = await response.text()
-            print(html)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(URL)
+        print(response.text)
+    page = BeautifulSoup(response.text, "lxml")
+    fix = page.find_all("li", class_="simple-match-cards-list__match-card")
+    print(fix)
 
 async def main():
     start = time.perf_counter()
-    URLS = ['https://www.premierleague.com/fixtures', 'https://www.skysports.com/la-liga-fixtures']
+    URLS = ['https://onefootball.com/en/competition/premier-league-9/fixtures', 'https://onefootball.com/en/competition/laliga-10/fixtures']
     coros = []
     for URL in URLS:
         coros.append(fetch_fixtures(URL))
